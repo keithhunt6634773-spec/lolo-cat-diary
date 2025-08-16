5dbf2b6d8be7c9213128f3c1b25720cf495a07c7
keithhunt6634773-spec <keithhunt6634773@gmail.com>
2025-08-16 23:39:05 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,39 +1,9 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <meta name="author" content="Dan Motzenbecker" />
-    <title>Lolo's 日记</title>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Google+Sans+Display:wght@400;700&family=Google+Sans+Mono&family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200&display=block"
-      rel="stylesheet"
-    />
-</head>
-  <body>
-    <div id="root"></div>
-    <script type="importmap">
-{
-  "imports": {
-    "react": "https://esm.sh/react",
-    "react/": "https://esm.sh/react/",
-    "react-dom": "https://esm.sh/react-dom",
-    "react-dom/": "https://esm.sh/react-dom/",
-    "recharts": "https://esm.sh/recharts",
-    "recharts/": "https://esm.sh/recharts/",
-    "@google/genai": "https://esm.sh/@google/genai@^1.14.0",
-    "react-syntax-highlighter/": "https://esm.sh/react-syntax-highlighter@^15.6.1/",
-    "react-syntax-highlighter": "https://esm.sh/react-syntax-highlighter@^15.6.1",
-    "clsx": "https://esm.sh/clsx@^2.1.1",
-    "zustand/": "https://esm.sh/zustand@^5.0.7/",
-    "zustand": "https://esm.sh/zustand@^5.0.7",
-    "immer": "https://esm.sh/immer@^10.1.1",
-    "p-limit": "https://esm.sh/p-limit@^7.0.0",
-    "auto-zustand-selectors-hook": "https://esm.sh/auto-zustand-selectors-hook@^3.0.1",
-    "lodash.shuffle": "https://esm.sh/lodash.shuffle@^4.2.0"
-  }
-}
-</script>
-    <script type="module" src="/index.tsx"></script>
-  </body>
-</html>
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+*/
+import {createRoot} from 'react-dom/client';
+import App from './src/components/App.jsx';
+import './main.css';
+
+createRoot(document.getElementById('root')).render(<App />);
