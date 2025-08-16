ec0c6288174c875c853c3d6d68a11fe9ae9f5b23
keithhunt6634773-spec <keithhunt6634773@gmail.com>
2025-08-16 23:47:36 +0800

    Create App.jsx

---
+++ App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Application Component</h1>
+      <p>The main App.jsx component has been successfully loaded.</p>
+    </div>
+  );
+}
+
+export default App;
