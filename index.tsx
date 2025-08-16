8f93429d25d19b74a5885f38fcc7adafba211427
keithhunt6634773-spec <keithhunt6634773@gmail.com>
2025-08-16 23:25:22 +0800

    Add files via upload

---
+++ index.tsx
@@ -0,0 +1,9 @@
+/**
+ * @license
+ * SPDX-License-Identifier: Apache-2.0
+*/
+import {createRoot} from 'react-dom/client';
+import App from './src/components/App.jsx';
+import './main.css';
+
+createRoot(document.getElementById('root')).render(<App />);
