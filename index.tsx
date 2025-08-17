1ede95c9bf10d0a82754ad2d635833f0b3e6beee
keithhunt6634773-spec <keithhunt6634773@gmail.com>
2025-08-17 08:28:46 +0800

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -2,6 +2,7 @@
  * @license
  * SPDX-License-Identifier: Apache-2.0
 */
+import React from 'react'; // <--- 请把这一行添加进去
 import {createRoot} from 'react-dom/client';
 import App from './App.jsx';
 import './main.css';
